2189ac2751fa895e4891116040992c08741a1d8a
Paul Honig <paul@etv.cx>
2025-02-01 18:13:19 +0100

    Added press left space, then right, to repeate space bar.

--- keyboards/tweetydabird/lotus58/keymaps/ranzboard/keymap.c
+++ keyboards/tweetydabird/lotus58/keymaps/ranzboard/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  KC_TRNS, KC_NO, KC_NO, KC_UP, KC_NO, KC_PGUP,                                              KC_PGUP, KC_NO, KC_UP, KC_LBRC, KC_RBRC, KC_BSLS,
                  KC_TRNS, KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                                        KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_ENT,
                  LSFT_T(KC_CAPS), KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_END,   KC_COPY, KC_PSTE,           KC_END, KC_PIPE, KC_SCLN, KC_COLN, KC_BSLS, RSFT(KC_CAPS),
-                 KC_TRNS, KC_TRNS, KC_TRNS,                                     KC_TRNS, KC_TRNS,           KC_TRNS, KC_TRNS, KC_TRNS
+                 KC_TRNS, KC_TRNS, KC_TRNS,                                     KC_TRNS, KC_SPC,           KC_TRNS, KC_TRNS, KC_TRNS
                  ),
 
     [2] = LAYOUT(KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                    KC_TRNS, KC_TRNS,           KC_TRNS, KC_P7, KC_P8, KC_P9, KC_PPLS, KC_DEL,
